657987ebac9ecbc7c5acff349d605e54b8c26011
eliseubaldo <eliseu_baldo@hotmail.com>
2015-08-18 15:36:31 -0400

    fixed php json

--- include.php
+++ include.php
@@ -1,16 +1,12 @@
 <?php
 
 
-// funcionando mas com escape chars no json final
-
 $data = file_get_contents('php://input');
 $inp = file_get_contents('contacts.json');
 $tempArray = json_decode($inp, true);
-array_push($tempArray, $data);
+$tempData = json_decode($data,true); // Had to decode $data coz php was escaping it
+array_push($tempArray, $tempData);
 $jsonData = json_encode($tempArray);
 file_put_contents('contacts.json', $jsonData);
 
-//works
-//$data = file_get_contents('php://input');
-//file_put_contents('contacts.json', $data);
 ?>
